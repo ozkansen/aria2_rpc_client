ef5c2e946fe27bb491b65abceae08f948ce9472b
Özkan ŞEN <ozkansen@gmail.com>
2021-12-12 15:30:57 +0300

    set parameter optional at exception

--- aria2_rpc_client/exceptions.py
+++ aria2_rpc_client/exceptions.py
@@ -1,7 +1,12 @@
+from typing import Optional
+
+
 class BaseClientException(Exception):
-    def __init__(self, msg: str, from_exc: Exception) -> None:
+    def __init__(self, msg: Optional[str] = None, from_exc: Optional[Exception] = None) -> None:
         self.msg = msg
         self.from_exc = from_exc
+
+        super(Exception, self).__init__(self.msg, self.from_exc)
 
 
 class ConnectionRefusedException(BaseClientException):
